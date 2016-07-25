89c52289366a97c71a23777cca36816119c10ec2
Istvan Dobos <istvan.dobos@artsalliancemedia.com>
2016-07-25 11:35:43 +0100

    Added stub for python functions; infancy for a DAG

--- sched_sync_dag.py
+++ sched_sync_dag.py
@@ -1,6 +1,13 @@
 from airflow import DAG
+from airflow.operators import PythonOperator, PostgresOperator
 
-SchedSyncDAG = DAG('Fake Schedule Sync')
+SchedSyncDAG = DAG('Fake Schedule Sync')  # TODO default args
 
-# let's try woth Python operators first
+# let's try with Python/Postgres operators first
+
+task_0_pull_pos_feed = PythonOperator(...)
+
+task_1_create_templates_for_schedules = PythonOperator(...)
+
+
 
---
+++ sched_sync_functions.py
@@ -0,0 +1,4 @@
+# Python functions used by Sched Sync are here
+
+def pull_pos_feed():
+    pass
