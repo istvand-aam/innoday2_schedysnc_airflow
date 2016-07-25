82237ac6a39468e8725d531531d0f6129b3809e8
Istvan Dobos <istvan.dobos@artsalliancemedia.com>
2016-07-25 11:22:46 +0100

    First additions yay

---
+++ sched_sync_dag.py
@@ -0,0 +1,6 @@
+from airflow import DAG
+
+SchedSyncDAG = DAG('Fake Schedule Sync')
+
+# let's try woth Python operators first
+
---
+++ sched_sync_op.py
@@ -0,0 +1,6 @@
+# There's going to be a sched_sync-related operator here
+
+from airflow.models import BaseOperator
+
+class DefineItOperator(BaseOperator):
+    pass
