2e4fa724185dccb3534b644d6aa350996ee9fd1b
Istvan Dobos <istvan.dobos@artsalliancemedia.com>
2016-07-25 13:38:12 +0100

    First complete graph (sort of)

---
+++ dags/sched_sync_dag.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators import PythonOperator, SimpleHttpOperator
+from src.sched_sync_functions import pull_pos_feed, create_playlists, create_schedules, cleanup_schedules
+
+SchedSyncDAG = DAG('Fake_Schedule_Sync')  # TODO default args?
+
+MANANA = (datetime.now() + timedelta(days=1))
+
+# let's try with Python and HTTP operators
+
+# result: pos sessions
+task_pull_pos_feed = PythonOperator(
+    task_id='pull_pos_feed',
+    python_callable=pull_pos_feed,
+    start_date=MANANA,
+    op_kwargs={},
+    dag=SchedSyncDAG
+)
+
+# result: SPLs with content
+task_create_playlists = PythonOperator(
+    task_id='create_playlist',
+    python_callable=create_playlists,
+    start_date=MANANA,
+    op_kwargs={},
+    dag=SchedSyncDAG
+)
+
+# result: schedules (shows) with assigned SPLs
+task_create_schedules = PythonOperator(
+    task_id='create_schedules',
+    python_callable=create_schedules,
+    start_date=MANANA,
+    op_kwargs={},
+    dag=SchedSyncDAG
+)
+
+task_send_schedules_to_screen = SimpleHttpOperator(
+    task_id='send_schedules_to_screen',
+    endpoint='127.0.0.1',
+    start_date=MANANA,
+    dag=SchedSyncDAG
+)
+
+# result: clean slate on the (sms of the) screen - no schedules
+task_cleanup_previous_schedules = PythonOperator(
+    task_id='cleanup_schedules',
+    python_callable=cleanup_schedules,
+    start_date=MANANA,
+    op_kwargs={},
+    dag=SchedSyncDAG
+)
+
+# Dependencies among tasks
+task_create_schedules.set_upstream(task_create_playlists)
+task_create_schedules.set_upstream(task_cleanup_previous_schedules)
+task_create_schedules.set_upstream(task_pull_pos_feed)
+# task_create_playlists.set_upstream(task_pull_pos_feed)
+task_send_schedules_to_screen.set_upstream(task_create_schedules)
--- sched_sync_functions.py
+++ sched_sync_functions.py
@@ -1,4 +1,18 @@
 # Python functions used by Sched Sync are here
 
+CREDENTIALS = {
+    'user': 'admin',
+    'password': 'admin'
+}
+
 def pull_pos_feed():
+    pass
+
+def create_playlists():
+    pass
+
+def create_schedules():
+    pass
+
+def cleanup_schedules():
     pass
