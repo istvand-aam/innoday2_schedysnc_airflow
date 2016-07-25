6168a33312e54c99d917ff8e9ce4642d4aef84a9
Istvan Dobos <istvan.dobos@artsalliancemedia.com>
2016-07-25 13:53:32 +0100

    Better pythonpath, http task fix

--- dags/sched_sync_dag.py
+++ dags/sched_sync_dag.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators import PythonOperator, SimpleHttpOperator
-from src.sched_sync_functions import pull_pos_feed, create_playlists, create_schedules, cleanup_schedules
+# you need to add stuff to pythonpath :(
+from sched_sync_functions import pull_pos_feed, create_playlists, create_schedules, cleanup_schedules
 
 SchedSyncDAG = DAG('Fake_Schedule_Sync')  # TODO default args?
 
@@ -36,6 +37,7 @@ task_create_schedules = PythonOperator(
     dag=SchedSyncDAG
 )
 
+# result: schedules (SPL-startdatetime-screenid) on the device
 task_send_schedules_to_screen = SimpleHttpOperator(
     task_id='send_schedules_to_screen',
     endpoint='127.0.0.1',
@@ -44,11 +46,10 @@ task_send_schedules_to_screen = SimpleHttpOperator(
 )
 
 # result: clean slate on the (sms of the) screen - no schedules
-task_cleanup_previous_schedules = PythonOperator(
+task_cleanup_previous_schedules = SimpleHttpOperator(
     task_id='cleanup_schedules',
-    python_callable=cleanup_schedules,
+    endpoint='127.0.0.1',
     start_date=MANANA,
-    op_kwargs={},
     dag=SchedSyncDAG
 )
 
@@ -56,5 +57,4 @@ task_cleanup_previous_schedules = PythonOperator(
 task_create_schedules.set_upstream(task_create_playlists)
 task_create_schedules.set_upstream(task_cleanup_previous_schedules)
 task_create_schedules.set_upstream(task_pull_pos_feed)
-# task_create_playlists.set_upstream(task_pull_pos_feed)
 task_send_schedules_to_screen.set_upstream(task_create_schedules)
